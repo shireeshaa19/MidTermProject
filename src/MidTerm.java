07d3dddda47ac78bd0b51257defa48b66e11e85a
David <dawatkins16@gmail.com>
2019-07-15 11:07:40 -0400

    changes

--- src/MidTerm.java
+++ src/MidTerm.java
@@ -2,12 +2,12 @@
 public class MidTerm {
 
 	public static void main(String[] args) {
-		System.out.println("Once upon a time in the mystical land of Shangrila there dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys.");
+		System.out.println("Once upon a time in the mystical land of Shangrila there dwelt an immortal named David and his trusty ninja students.");
 		System.out.println("They were very clever monkeys indeed, known for their ingenious teamwork.");
-		System.out.println("The monkeys loved to eat bananas. but their blood-lust was far superior to their love of bannanas. They would take down entire civilizations just for a taste of flesh.");
-		System.out.println("James then took on the monkeys with his banana shorts!");
+		System.out.println("The students loved to eat bananas. But their blood-lust was far superior to their love of bannanas. They would take down entire civilizations just for a taste of flesh.");
+		System.out.println("James then took on the students with his banana shorts!");
 		System.out.println("Once they were challenged by a swarm of seemingly unstoppable bugs.");
-		System.out.println("James was no match for the bugs and monkeys. They tore into him with ease. James screamed out for Kyle, but Kyle knew James had less than a 60% so saving hims was a mute point. So Kyle picked up David (Who had an 80% and chose to save him instead.");
+		System.out.println("James was no match for the bugs and students. They tore into him with ease. James screamed out for Kyle, but Kyle knew James had less than a 60% so saving hims was a mute point. So Kyle picked up David (Who had an 80% and chose to save him instead.");
 	}
 
 }
