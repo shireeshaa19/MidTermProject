f5b11e7e5f1b8cd534f97970f2b7417bee2a8ecf
David <dawatkins16@gmail.com>
2019-07-15 10:48:51 -0400

    banana pants

--- src/MidTerm.java
+++ src/MidTerm.java
@@ -5,6 +5,7 @@ public class MidTerm {
 		System.out.println("Once upon a time in the mystical land of Shangrila there dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys.");
 		System.out.println("They were very clever monkeys indeed, known for their ingenious teamwork.");
 		System.out.println("The monkeys loved to eat bananas. but their blood-lust was far superior to their love of bannanas. They would take down entire civilizations just for a taste of flesh.");
+		System.out.println("James then took on the monkeys with his banana shorts!");
 	}
 
 }
