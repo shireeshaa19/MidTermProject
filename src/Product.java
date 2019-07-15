ff048de7aad5b47501307c476cd954da770cdae4
shireeshaa19 <amancha.shireesha1@gmail.com>
2019-07-15 13:22:25 -0400

    product class commit

---
+++ src/Product.java
@@ -0,0 +1,59 @@
+
+public class Product {
+	
+	private String name;
+	private String category;
+	private String description;
+	private double price;
+	
+	public Product(){
+		super();
+	}
+	
+	public Product(String name,String category,String description,double price){
+		this.name = name;
+		this.category = category;
+		this.description = description;
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return (getName() + "\t " + getCategory() + " \t " + getDescription() + " \t " + getPrice());
+			
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+
+}
