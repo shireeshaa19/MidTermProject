dcb832be2919b2fa8f18d9ef8cdd6bac89473788
jrw0594 <jrw0594@gmail.com>
2019-07-15 10:57:46 -0400

    final choice

--- src/MidTerm.java
+++ src/MidTerm.java
@@ -7,6 +7,7 @@ public class MidTerm {
 		System.out.println("The monkeys loved to eat bananas. but their blood-lust was far superior to their love of bannanas. They would take down entire civilizations just for a taste of flesh.");
 		System.out.println("James then took on the monkeys with his banana shorts!");
 		System.out.println("Once they were challenged by a swarm of seemingly unstoppable bugs.");
+		System.out.println("James was no match for the bugs and monkeys. They tore into him with ease. James screamed out for Kyle, but Kyle new James had less than a 60% so saving hims was a mute point. So Kyle picked up David (Who had an 80% and chose to save him instead.");
 	}
 
 }
